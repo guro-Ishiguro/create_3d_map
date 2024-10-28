511dbf0df2d93c2e8a5ee9ed50171a6c3b981131
guro-Ishiguro <guro120411@gmail.com>
2024-10-28 15:20:10 +0900

    add cuda test file

---
+++ test/cuda_test.py
@@ -0,0 +1,6 @@
+import torch
+
+print("CUDA is available:", torch.cuda.is_available())
+print("Device count:", torch.cuda.device_count())
+if torch.cuda.is_available():
+    print("Device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
