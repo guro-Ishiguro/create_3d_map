2f909e108fc58b8f1718abf00728ccdf56f2a58b
guro-Ishiguro <guro120411@gmail.com>
2024-10-30 01:48:04 +0900

    fix/orthorectify

--- test/create_disparity_image.py
+++ test/create_disparity_image.py
@@ -22,18 +22,21 @@ def create_disparity_image(image_L, image_R, window_size, min_disp, num_disp):
     disparity = stereo.compute(image_L, image_R).astype(np.float32) / 16.0
     return disparity
 
-def to_orthographic_projection(disparity, principal_point_distance):
+def to_orthographic_projection(depth, camera_height):
     """中心投影から正射投影への変換を適用する"""
-    rows, cols = disparity.shape
+    rows, cols = depth.shape
     mid_idx = cols // 2
-    col_indices = np.arange(cols)  
-    row_indices = np.arange(rows)[:, None]  
-    shift = (disparity * (mid_idx - col_indices) / (principal_point_distance + disparity)).astype(int)
-    new_col_indices = col_indices + shift
-    new_col_indices = np.clip(new_col_indices, 0, cols - 1)
-    shifted_disparity = np.zeros_like(disparity)
-    shifted_disparity[row_indices, new_col_indices] = disparity
-    return shifted_disparity
+    mid_idy = rows // 2
+    col_indices = np.vstack([np.arange(cols)] * rows)
+    row_indices = np.vstack([np.arange(rows)] * cols).transpose()
+    shift_x = ((camera_height - depth) * (mid_idx - col_indices) / camera_height).astype(int)
+    shift_y = ((camera_height - depth) * (mid_idy - row_indices) / camera_height).astype(int)
+    new_x = np.clip(col_indices + shift_x, 0, cols - 1)
+    new_y = np.clip(row_indices + shift_y, 0, rows - 1)
+    ortho_depth = np.full_like(depth, np.inf)
+    np.minimum.at(ortho_depth, (new_y, new_x), depth)
+    ortho_depth[ortho_depth == np.inf] = 0
+    return ortho_depth
 
 def save_disparity_colormap(disparity, output_path):
     """視差画像をカラーマップで保存する"""
@@ -49,18 +52,18 @@ def save_disparity_colormap(disparity, output_path):
     print(f"Disparity colormap saved as {output_path}")
 
 # 左右の画像を読み込み
-img_id = 2000
+img_id = 1001
 left_image = cv2.imread(os.path.join(DRONE_IMAGE_DIR, f"left_{img_id}.png"), cv2.IMREAD_GRAYSCALE)
 right_image = cv2.imread(os.path.join(DRONE_IMAGE_DIR, f"right_{img_id}.png"), cv2.IMREAD_GRAYSCALE)
 
 B, fov_v, height = 0.3, 60, 1440
 focal_length = height / (2 * np.tan(fov_v * np.pi / 180 / 2))
-principal_point_distance = principal_point_distance = B * focal_length / 20
+camera_height = 20
 
 # 視差画像を生成
 disparity = create_disparity_image(left_image, right_image, window_size=5, min_disp=0, num_disp=80)
 
-disparity = to_orthographic_projection(disparity, principal_point_distance)
+disparity = to_orthographic_projection(disparity, camera_height)
 
 # 視差画像をカラーマップとして保存
 output_path = os.path.join(DISPARITY_IMAGE_DIR, f"disparity_{img_id}.png")
